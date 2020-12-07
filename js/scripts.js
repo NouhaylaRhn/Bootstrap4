6b85d9705b5c99435734827502314662f8abcd1a
NouhaylaRhn <nouhayla.erryhany@gmail.com>
2020-12-07 12:29:29 +0000

    Building

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+        $(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 1000 } );
+            $("#carouselButton").click(function(){
+                if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carouselButton").children("span").removeClass('fa-pause');
+                    $("#carouselButton").children("span").addClass('fa-play');
+                }
+                else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carouselButton").children("span").removeClass('fa-play');
+                    $("#carouselButton").children("span").addClass('fa-pause');                    
+                }
+            });
+
+            $("#buttonReserve").click(function() {
+            $('#reservemodel').show();
+            });
+
+
+            $("#loginbutton").click(function() {
+            $('#loginModel').show();
+            });
+
+        });
+
+
+
